e1bf66cbb9e4a24665cc4f82faa4bb0cbf34cba5
IvanFomin <vanyafomin@yandex.ru>
2020-09-26 00:24:08 +0300

    Create add register communication line

--- Model_app/Application.py
+++ Model_app/Application.py
@@ -8,8 +8,10 @@ Created on Fri Sep 18 16:27:40 2020
 from Model import Model
 from Signal import Signal
 from Modem import Modem
+from Line import CommLine
 
 Modem = Modem()
+NKP = CommLine()
 
 Model.signal.time = 10
 Model.signal.amplitude = 1
@@ -22,3 +24,13 @@ Modem.unit_time = 1
 #Model.signal.Simple()
 Modem.FM_2()
 Model.signal.Plot()
+
+# Обертка канала связи
+NKP.change_parameters(input_signal = Model.signal.value, 
+      				  type_of_line = 'gauss', dispersion = 1, mu = 0)
+Model.signal.value = NKP.output
+
+# Просто дебаг информация
+NKP.print_input()
+print("\n\n")
+NKP.print_output()
--- Model_app/Line.py
+++ Model_app/Line.py
@@ -1,4 +1,90 @@
-class CommLine(Model):
+"""
+Модуль линии связи:
+Работает как черный ящик с многими входными данными и с одним выходом
+Вход: kwargs - словарь параметров и их значений для настройки линии связи,
+а также может содержать входные данные для работы с ними
+Выход: output - выходной сигнал
+С внешней стороны доступно только поле output и метод change_parameters
+"""
 
-	def __init__(self):
+import random
+
+class CommLine():
+
+	def __init__(self, **kwargs):
+
+		# Init new input signal and output
+		self.__input = []
+		self.output = []
+
+		# Communication line noise parameters
+		self.__type_of_line = 0
+		self.__dispersion = 0
+		self.__mu = 0
+
+		# Load setup methods
+		self.change_parameters(**kwargs)
+
+	def change_parameters(self, **kwargs):
+
+		# Search right keys in input parameters 
+		for key in kwargs:
+			# Load new input signal
+			if key == 'input_signal':
+				self.__input = kwargs[key]
+				self.output = []
+
+			# Switch to choose type of communication line
+			if key == 'type_of_line':
+				if kwargs[key] == 'gauss':
+					self.__type_of_line = 1
+				elif kwargs[key] == 'line_distor':
+					self.__type_of_line = 2
+				elif kwargs[key] == 'garmonic':
+					self.__type_of_line = 3
+				elif kwargs[key] == 'relei':
+					self.__type_of_line = 4
+
+			# Change init parameters of noise
+			elif key == 'dispersion':
+				self.__dispersion = kwargs[key]
+			elif key == 'mu':
+				self.__mu = kwargs[key]
+
+		# Link to work methods
+		self.__choose_mode()
+
+	# Switch to choose work method
+	def __choose_mode(self):
+		if self.__type_of_line == 0:
+			self.output = self.__input.copy()
+		elif self.__type_of_line == 1:
+			self.__gauss()
+		elif self.__type_of_line == 2:
+			self.__line_distor()
+		elif self.__type_of_line == 3:
+			self.__garmonic()
+		elif self.__type_of_line == 4:
+			self.__relei()
+
+	# Work methods
+	def __gauss(self):
+		for i in range(len(self.__input)):
+			self.output.append(self.__input[i] + \
+			random.gauss(self.__mu, self.__dispersion))
+
+	def __line_distor(self):
 		pass
+
+	def __garmonic(self):
+		pass
+
+	def __relei(self):
+		pass
+
+	# Debug methods
+	def print_input(self):
+		print(self.__input)
+
+	def print_output(self):
+		print(self.output)
