709936485c451741dbe6aeb4025ee9ec2a80c5f0
TritonVera <vanyafomin@yandex.ru>
2020-11-22 00:41:07 +0300

    New

---
+++ Model_app/OTiTPRI.py
@@ -0,0 +1,24 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from UI import DemoWindow as UI
+from Modem import Modem
+from Model import Model
+from Line import CommLine
+from Controller import Controller
+from Processor import Processor
+
+Modem = Modem()
+Processor = Processor()
+Model.signal.frequency = 1
+NKP = CommLine()
+app = QApplication(sys.argv)
+ui = UI()
+
+manage = Controller(ui, Modem, NKP, Processor)
+ui.button_panel.plot_button.clicked.connect(manage.plot_view)
+ui.line_panel.combobox.activated.connect(manage.show_param)
+ui.error_panel.combobox.activated.connect(manage.show_error)
+
+ui.show()
+sys.exit(app.exec_())
